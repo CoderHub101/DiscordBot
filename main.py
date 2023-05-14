76cb8f335be03bb842db55d27970b69ddc79762a
iTakedown27 <ezrabridger27@gmail.com>
2023-05-13 22:13:02 -0700

    comments

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ file = open('config.json')
 info = json.load(file)
 config = info['token']
 
-import discord
-
+# enabling intents
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+# Startup log
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+# Message event
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
+    if message.content.lower().startswith('hello') or message.content.lower().startswith('hi') or message.content.lower().startswith('hey'):
         await message.channel.send('Hello!')
         
+# Initiates bot
 client.run(config)
