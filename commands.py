6eab3b4ddae7e49d994aea1a9a4ae2ddba932f06
iTakedown27 <ezrabridger27@gmail.com>
2023-05-18 21:53:11 -0700

    Updated more functions
    Added a dictionary for the functions and descriptions, bot shuts down upon saying bye, and need to get the help function to work

--- commands.py
+++ commands.py
@@ -1,7 +1,15 @@
 import random
 
+commandList = {'codinglang':'Selects a random coding language.', 'coinflip': 'Flips a coin.'}
 proglangs = ['Python', 'JavaScript', 'Java', 'C', 'C++', 'C#', 'Go', 'Rust', 'Swift', 'Kotlin', 'Scala', 'PHP']
 
 # Gives random programming language
 def random_lang():
     return proglangs[random.randint(0, len(proglangs))]
+
+def coinflip():
+    rand = random.randint(0, 2)
+    if rand == 1:
+        return "Heads"
+    else:
+        return "Tails"
--- main.py
+++ main.py
@@ -28,20 +28,37 @@ async def on_ready():
 async def on_message(message):
 
     # TODO: Can we make a dictionary associating these message
-    # contents with their specific actions instead of brute
+    # contents with their specific functions instead of brute
     # force code?
+
+    if message.content.lower().startswith(prefix):
     
-    if message.author == client.user:
-        return
+        if message.author == client.user:
+            return
 
-    if 'hello' in message.content.lower() or 'hi' in message.content.lower()or 'hey' in message.content.lower():
-        await message.channel.send('Hello!')
+        if 'hello' in message.content.lower() or 'hi' in message.content.lower()or 'hey' in message.content.lower():
+            await message.channel.send('Hello!')
 
-    if 'codinglang' in message.content.lower():
-        await message.channel.send(commands.random_lang())
+        if 'codinglang' in message.content.lower():
+            await message.channel.send(commands.random_lang())
 
-    if 'bye' in message.content.lower():
-        await message.channel.send('Bye bye!')
+        if 'bye' in message.content.lower():
+            await message.channel.send('Bye bye!')
+            print('Shutting down!')
+            await client.close()
+
+        if 'coinflip' in message.content.lower():
+            result = commands.coinflip()
+            await message.channel.send(result)
+
+        if 'purge50' in message.content.lower():
+            for i in range(0, 50):
+                await discord.Message.delete()
+
+        # TODO: Fix this, not printing commands
+        if 'help' in message.content.lower():
+            for command, description in commands.commandList:
+                await message.channel.send(f'{prefix} {command} - {description}\n')
         
 # Initiates bot, log handler, and debugs unexpected errors
 client.run(config, log_handler=handler, log_level=logging.DEBUG)
